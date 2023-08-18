79fe399d9f556663ccf202f60e5a119bab3f369d
juan-camilo-rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2023-08-17 19:31:58 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-/*Código que calcula el recorrido que debe seguir un caballo en un tablero de ajedrez nxn para pasar por cada casilla sin repetir ninguna*/
-
-// Hecho por Juan Camilo Rosero
-
-let paso = 0,
-n = 6,
-total = (n*n)-1,
-pasos_Eliminados = [[]],
-posicion = [0,0],
-camino = [posicion],
-tablero = [],
-contador = 0,
-intervalo = 500,
-casillas_Vacias = 0,
+const d = document,
+$btn = d.querySelector(".start"),
+camino = []
+let n = 0,
+total = 0,
 blanco = true
-
-const contieneArreglo = (arr, arregloPrincipal) => {
-    return arregloPrincipal.some(subArr => JSON.stringify(subArr) === JSON.stringify(arr)); // Línea hecha con ChatGPT
-}
 
 const crear_Tablero = n => {
     const $tablero = document.querySelector(".tablero")
@@ -28,9 +15,7 @@ const crear_Tablero = n => {
         }
         const $fila = document.createElement("div")
         $fila.classList.add("fila")
-        tablero.push([])
         for (let j = 0; j < n; j++){
-            tablero[i].push([i,j])
             const $cuadro = document.createElement("div"),
             $texto = document.createElement("p")
             //$texto.textContent = i + ", " + j
@@ -44,7 +29,7 @@ const crear_Tablero = n => {
                 $cuadro.classList.add("gris")
                 blanco = true
             }
-            $cuadro.setAttribute("data-pos", i + "-" + j)
+            $cuadro.setAttribute("data-pos", `${i}${j}`)
             $cuadro.appendChild($texto)
             $fila.appendChild($cuadro)
         }
@@ -52,9 +37,13 @@ const crear_Tablero = n => {
     }
 }
 
+const contieneArreglo = (arr, arreglo_Principal) => {
+    return arreglo_Principal.some(subArr => JSON.stringify(subArr) === JSON.stringify(arr)); // Línea hecha con ChatGPT
+}
+
 const rellenar_Tablero = caminoFinal => {
     caminoFinal.forEach((pos, i) => {
-        const atributo = pos[0] + "-" + pos[1],
+        const atributo = `${pos[0]}${pos[1]}`,
         $cuadro = document.querySelector(`[data-pos="${atributo}"]`),
         $texto = document.querySelector(`[data-pos="${atributo}"] p`)
         $texto.textContent = i + 1
@@ -62,9 +51,11 @@ const rellenar_Tablero = caminoFinal => {
 }
 
 const posibles_Opciones = posicion => {
+    //console.log(posicion);
     const opciones = [],
     fila = posicion[0],
-    columna = posicion[1]
+    columna = posicion[1],
+    opciones_Validas = []
 
     opciones.push([fila + 1, columna + 2])
     opciones.push([fila + 1, columna - 2])
@@ -75,24 +66,10 @@ const posibles_Opciones = posicion => {
     opciones.push([fila - 2, columna + 1])
     opciones.push([fila - 2, columna - 1])
 
-    return opciones
-}
-
-const probar_Opciones = opciones => {
-    const opciones_Validas = []
-    /*console.log("*-*-");
-    console.log(camino);
-    console.log(paso);
-    console.log(pasos_Eliminados[paso]);*/
     opciones.forEach((opcion, i) => {
         const fila = opcion[0],
         columna = opcion[1]
-        if(fila >= 0 && fila < n && columna >= 0 && columna < n && contieneArreglo(opcion, camino) == false && contieneArreglo(opcion, pasos_Eliminados[paso]) == false){
-            /*console.log("-----");
-            console.log(opcion);
-            console.log(camino);
-            console.log("-----");*/
-            //console.log(opcion + " es valida");
+        if(fila >= 0 && fila < n && columna >= 0 && columna < n && contieneArreglo(opcion, camino) == false){
             opciones_Validas.push(opcion)
         }
     });
@@ -100,58 +77,36 @@ const probar_Opciones = opciones => {
     return opciones_Validas
 }
 
-let camino_Caballo = () => {
-    //console.log(pasos_Eliminados);
-    if (paso != total - casillas_Vacias || contador == 20) {
-        const pasos_Disponibles = probar_Opciones(posibles_Opciones(posicion))
-        if (pasos_Disponibles.length == 0){
-            pasos_Eliminados.pop()
-            pasos_Eliminados[pasos_Eliminados.length - 1].push(camino[camino.length - 1])
-            camino.pop()
-            posicion = camino[camino.length - 1]
-            paso -= 1
+const probar_Opciones = posicion_Actual => {
+    let opciones_A_Probar = posibles_Opciones(posicion_Actual),
+    mejor_Camino = opciones_A_Probar[0],
+    mejor_Cantidad = 0
+    opciones_A_Probar.forEach(opcion => {
+        let mayor_Opcion = posibles_Opciones(opcion)[0]
+        posibles_Opciones(opcion).forEach(opcion_De_Opcion => {
+            if(posibles_Opciones(mayor_Opcion).length < posibles_Opciones(opcion_De_Opcion).length) mayor_Opcion = opcion_De_Opcion
+        });
+        if(mayor_Opcion != undefined){
+            if(mejor_Cantidad < posibles_Opciones(mayor_Opcion).length){
+                mejor_Cantidad = posibles_Opciones(mayor_Opcion).length
+                mejor_Camino = opcion
+            }
         }
-        else{
-            //console.log("ejecutando else");
-            posicion = pasos_Disponibles[0]
-            camino.push(posicion)
-            pasos_Eliminados.push([])
-            paso++
-        }
-        contador++
-        camino_Caballo()
-    }
+        else console.log("Me quedé sin movimientos en ésta casilla :<");
+    });
+    return mejor_Camino
 }
-
-function descansar() {
-    let interval = setInterval(() => {
-        if (paso != total - casillas_Vacias){
-            console.log("Descansemos un poquito :3")
-            contador = 0
-            camino_Caballo()
-        }
-        else {
-            clearInterval(interval)
-            console.log("TERMINE :D");
-            rellenar_Tablero(camino)
-            console.log(camino);
-        }
-    }, intervalo);
-    
-}
-
-const $n = document.querySelector(".n"),
-$casillas_Vacias = document.querySelector(".casillas-vacias"),
-$pos_X = document.querySelector(".pos-x"),
-$pos_Y = document.querySelector(".pos-y"),
-$btn = document.querySelector(".start")
 
 $btn.addEventListener("click", e => {
-    n = parseInt($n.value)
-    total = (n*n)-1
-    casillas_Vacias = parseInt($casillas_Vacias.value)
-    posicion = [parseInt($pos_Y.value), parseInt($pos_X.value)]
-    camino = [posicion]
+    n = d.querySelector(".n").value
+    total = (n*n) - 1
+    let posicion = [0,0]
     crear_Tablero(n)
-    descansar()
+    camino.push(posicion)
+    for (let i = 0; i < total; i++) {
+        let siguiente_Paso = probar_Opciones(posicion)
+        camino.push(siguiente_Paso)
+        posicion = siguiente_Paso
+        rellenar_Tablero(camino)
+    }
 })
